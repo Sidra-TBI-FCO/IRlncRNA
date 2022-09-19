1a97adf114ed0ee35024b10375efef7c21c2ce94
Shimaa Sherif Khedr <56906347+shimaakhedr@users.noreply.github.com>
2022-09-19 08:55:02 +0300

    Update and rename Figure 2.Normalization and limma.DELnRNAs.TCGA.BRCA.R to 01.Normalization and limma.DELnRNAs.TCGA.BRCA.R

--- R scripts/01.Normalization and limma.DELnRNAs.TCGA.BRCA.R
+++ R scripts/01.Normalization and limma.DELnRNAs.TCGA.BRCA.R
@@ -82,29 +82,3 @@ diff.genes = diff.stats[diff.stats$adj.P.Val < 0.05,]
 dir.create(paste0("./Analysis/DEG"), showWarnings = FALSE)
 
 write.csv(diff.stats, file = paste0("./Analysis/DEG/diff_lncRNAs_TCGA.csv"))
-
-## Create the volcano plot 
-
-#load data
-DIFF = read.csv("./Analysis/DEG/diff_lncRNAs_TCGA.csv")
-
-DIFF$gene_name = str_trim(DIFF$gene_name)
-
-dev.new()
-
-svg(filename = paste0("./Figures/Volcano.DELncRNAs.limma.TCGA.labelled.svg"), width = 7, height = 7,pointsize = 12)
-
-# Creating the Volcano plot
-EnhancedVolcano(DIFF,
-                lab = NA,
-                x = 'logFC',
-                y = 'adj.P.Val',
-                pCutoff = 0.05,FCcutoff = 1,
-                selectLab = DIFF$gene_name[DIFF$adj.P.Val<= 1e-12 & abs(DIFF$logFC) >= 1],
-                labSize = 6.0,
-                pointSize = 2.0,
-                gridlines.major = FALSE,
-                gridlines.minor = FALSE,
-)
-
-dev.off()
